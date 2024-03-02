52d22dc09509bdcdadad5f09c42cb93b2888783a
dnlmsnv <danya.misinev666gmail.com@mail.ru>
2024-03-02 12:17:58 +0700

    it is the correct code hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
-using namespace std;
-int main()
+
+int main(int /*argc*/,char* /*argv*/[])
 {
     string name=nullptr;
     cout<<"Введите имя пользователя"<<endl;
