fcb4b91dfd06603f4267ef0aac90a88d86d6408f
dnlmsnv <danya.misinev666gmail.com@mail.ru>
2024-03-02 13:04:27 +0700

    with comms

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream> /*cтандартные потоки ввода-вывода*/
 #include <string>
 
 int main(int /*argc*/,char* /*argv*/[])
 {
-    string name=nullptr;
-    cout<<"Введите имя пользователя"<<endl;
+    string name=nullptr; /*инициализация переменной*/
+    cout<<"Введите имя пользователя"<<endl; /*приглашение на ввод*/
     cin>>name;
-    cout<<"Hello world!"<<" "<<name<<endl;
+    cout<<"Hello world!"<<" "<<name<<endl; /*вывод*/
     return 0;
 }
